b09840148222c016f556bd0f00b20ce203d323ca
lumusitech <lumusitech@gmail.com>
2021-09-21 23:00:48 -0300

    [ADD] mongodb config added

---
+++ db/config.mongodb.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mongoConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Mongo DB connected');
+    } catch (error) {
+        console.log(`erros founded at try connect mongo db:\n${error}`);
+    }
+}
+
+module.exports = mongoConnect;
