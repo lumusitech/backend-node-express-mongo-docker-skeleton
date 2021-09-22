bc6d9962f7ccc4b4eda2f635afabe1c1e609ff46
lumusitech <lumusitech@gmail.com>
2021-09-21 23:21:14 -0300

    [EDIT] server created into main app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-console.log('setted first configs successfuly!!');
+const Server = require('./models/server');
 
 console.log(process.env.APP_NAME);
 
 console.log(process.env.NODE_ENV);
+
+const server = new Server();
+
+server.start();
