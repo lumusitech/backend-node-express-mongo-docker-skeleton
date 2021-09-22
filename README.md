81ae3efa828522ad36d72f9e2cefc185cf4a2127
lumusitech <lumusitech@gmail.com>
2021-09-21 22:43:09 -0300

    [ADD] base project added

---
+++ README.md
@@ -0,0 +1,7 @@
+# RestServer with node, express, docker-compose and mongo
+
+After clone this repo, run:
+
+```sh
+npm i
+```
---
+++ src/app.js
@@ -0,0 +1,5 @@
+console.log('setted first configs successfuly!!');
+
+console.log(process.env.APP_NAME);
+
+console.log(process.env.NODE_ENV);
